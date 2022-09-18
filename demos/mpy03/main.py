f79ed07fed31608f24eee875037554568b40660e
Ruben Acuña <racuna1@asu.edu>
2022-09-18 16:33:14 -0700

    MPY03: added outline for next scatterplot examples.

--- demos/mpy03/main.py
+++ demos/mpy03/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+__author__      = "Ruben Acuna"
+__copyright__   = "Copyright 2022, Ruben Acuna"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#https://github.com/matplotlib/AnatomyOfMatplotlib/blob/master/AnatomyOfMatplotlib-Part1-Figures_Subplots_and_layouts.ipynb
+fig = plt.figure(facecolor=(1, 0, 0, .1))
+plt.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# what is format of color?
+# axes is an object.
+# fig contains axes contains axis+axis
+# define to define: figure, axes, axis, and relationships between them.
+
+fig = plt.figure(figsize=plt.figaspect(2.0), facecolor=(1, 0, 0, .1))
+plt.show()
+
+print(plt.figaspect(2.0)) # width, height
+
+
+fig = plt.figure()
+fig2 = plt.figure()
+ax = fig.add_subplot(111) # We'll explain the "111" later. Basically, 1 row and 1 column.
+ax2 = fig2.add_subplot(111)
+ax.set(xlim=[0.5, 4.5], ylim=[-2, 8], title='An Example Axes',
+       ylabel='Y-Axis', xlabel='X-Axis')
+ax2.set(xlim=[0.5, 4.5], ylim=[-2, 8], title='An Example Axes 2',
+       ylabel='Y-Axis', xlabel='X-Axis')
+# set position?
+
+# see note about the two different ways to do this.
+# ^ that has exceptions though.
+# For example, foo.set(bar='blah') would call foo.set_bar('blah').
+#  For example, when calling plt.xlim(1, 10), pyplot calls ax.set_xlim(1, 10) on whichever Axes is "current".
+plt.show()
+
+#multi plot example
+fig, axes = plt.subplots(nrows=2, ncols=2)
+axes[0,0].set(title='Upper Left')
+axes[0,1].set(title='Upper Right')
+axes[1,0].set(title='Lower Left')
+axes[1,1].set(title='Lower Right')
+for ax in axes.flat:
+    # Remove all xticks and yticks...
+    ax.set(xticks=[], yticks=[])
+plt.show()

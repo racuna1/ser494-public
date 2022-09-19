4293c63121a2478d1f46ab79183b41ffb2d45ac5
Ruben Acuña <racuna1@asu.edu>
2022-09-18 17:27:21 -0700

    MPY03: added scatterplot with more options.

--- demos/mpy03/demo_complete.py
+++ demos/mpy03/demo_complete.py
@@ -15,7 +15,6 @@ ax.set(title='Fancy Title',
 fig.show()
 fig.savefig("mpy03_demo_blankplot.png")
 
-
 ################################################################################
 # Line Plot
 # new topics: np.arange, latex in label, plot
@@ -37,34 +36,44 @@ fig.savefig("mpy03_demo_lineplot1.png")
 # new topics: np.linspace, default interpolation, multiple plots, legend, dots
 n2 = np.linspace(0, 10, 5)
 
-print(len(n2))
-print(n2)
-
 fig, ax = plt.subplots()
 ax.set(title='Growth Functions',
-       ylabel='Number of Operations', xlabel='X-Axis Nothingness')
+       ylabel='Number of Operations', xlabel='Problem Size')
 plt.plot(n, 2 * n ** 2 + 3, label="$f(n)=2n^2+3$ (40 samples)")
 plt.plot(n2, 2 * n2 ** 2 + 3, label=f"$f(n)=2n^2+3$ ({len(n2)} samples)")
 plt.plot(n2, 3 * n2 + 10, "o", label="$f(n)=3n+10$ (dot plot)")  # see Format Strings
 ax.legend()
 fig.show()
 fig.savefig("mpy03_demo_lineplot2.png")
-exit()
-
 
 
 ################################################################################
-# making look nice
-# TODO: subplots(figsize)
-# TODO: adding text on figure
-# TODO: teach axis limits
-# TODO: manual axis
-# FUTURE: text formatting
+# Line Plot (better visuals)
+# new topics: subplots(figsize), suptitle (fontsize), xlim/ylim, line formatting, text (fontstyle, alpha)
+# https://matplotlib.org/stable/api/colors_api.html#module-matplotlib.colors
+
+fig, ax = plt.subplots(figsize=(5, 4))
+fig.suptitle('Growth Functions', fontsize=20, color="black")
+ax.set(xlim=[0, 8], ylim=[0, 175],
+       ylabel='Number of Operations', xlabel='Problem Size')
+# alternative:
+# ax.set_xlim([0, 8])
+# ax.set_ylim([0, 175])
+# ax.set_ylabel('Number of Operations')
+# ax.set_xlabel('Problem Size')
+ax.plot(n, 2 * n ** 2 + 3, "--g", label="$f(n)=2n^2+3$ (40 samples)")
+ax.plot(n2, 2 * n2 ** 2 + 3, "cyan", label=f"$f(n)=2n^2+3$ ({len(n2)} samples)")
+ax.plot(n2, 3 * n2 + 10, "*", label="$f(n)=3n+10$ (dot plot)")
+ax.legend()
+ax.text(1.75, 24, "intersection", fontstyle="italic", alpha=.5)
+fig.show()
+fig.savefig("mpy03_demo_lineplot3.png")
+exit()
+
+# FUTURE: manual axis ticks
 # FUTURE: adding annotation?
-
-# TODO: line styles (shape + color + dot)
-
 # FUTURE: color scale?
+# TODO: multiple plots
 
 ################################################################################
 
@@ -100,7 +109,7 @@ ax.scatter(sp_a, sp_b)
 fig.show()
 fig.savefig("mpy03_demo_scatterplot1.png")
 
-# color scale?
+# TODO: color scale?
 
 ################################################################################
 # Scatter plot (four variables)
---
+++ demos/mpy03/demo_template.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+__author__      = "Ruben Acuna"
+__copyright__   = "Copyright 2022, Ruben Acuna"
+
+# this file contains an outline for the code demonstrations in the slides. it is
+# suggested that you load up this file at the start of lecture and then fill it
+# out as the lecture progresses. use it to play with the specific features we
+# discuss. this file is NOT graded.
+
+
+################################################################################
